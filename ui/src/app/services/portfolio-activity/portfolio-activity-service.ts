83f5c0e7417a7dc606d1c6fb969ce8f51cb6756c
baosiek <baosiek@gmail.com>
2025-09-23 18:39:23 -0700

    getSymbolsTotalsByPortfolio method implemented

--- ui/src/app/services/portfolio-activity/portfolio-activity-service.ts
+++ ui/src/app/services/portfolio-activity/portfolio-activity-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { PortfolioActivityMode, PortfolioActivityModel } from '../../models/portfolio-activity-model';
+import { PortfolioActivityMode, PortfolioActivityModel, SymbolByPortfolioTotalsModel } from '../../models/portfolio-activity-model';
 import { Observable } from 'rxjs';
 import { ReturnMessage } from '../../models/return-message';
 
@@ -25,6 +25,11 @@ export class PortfolioActivityService {
   getActivityForPortfolio(user_id: number, portfolio_id: number): Observable<PortfolioActivityModel[]> {
     const apiMethod = 'get_all';
     return this.http.get<PortfolioActivityModel[]>(`${this.apiUrl}/${apiMethod}/?user_id=${user_id}&portfolio_id=${portfolio_id}`)
+  }
+
+  getSymbolsTotalsByPortfolio(user_id: number, portfolio_id: number) : Observable<SymbolByPortfolioTotalsModel[]> {
+    const apiMethod = 'get_symbols_by_portfolio';
+    return this.http.get<SymbolByPortfolioTotalsModel[]>(`${this.apiUrl}/${apiMethod}/?user_id=${user_id}&portfolio_id=${portfolio_id}`)
   }
 
   deleteActivityForPortfolio(id: number) {
