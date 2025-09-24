bf4a7aa4370dd1b342d0fa337a435a305a0e1a7c
baosiek <baosiek@gmail.com>
2025-09-23 18:40:19 -0700

    initialized

---
+++ ui/src/app/components/dialogs/add-income-dialog/add-income-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-income-dialog',
+  imports: [],
+  templateUrl: './add-income-dialog.html',
+  styleUrl: './add-income-dialog.scss'
+})
+export class AddIncomeDialog {
+
+}
