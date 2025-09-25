fca4883fcacd9cf452bc4750ebd877d3c4b28557
baosiek <baosiek@gmail.com>
2025-09-25 11:26:24 -0700

    included snackbar defaults

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideNativeDateAdapter(),
+     {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false, maxWidth: '90vw', maxHeight: '90vh'}},
+     {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 5000}}
   ]
 };
