cca847043469f49f0ee4a5593231a99f57fbd1c7
baosiek <baosiek@gmail.com>
2025-09-28 12:41:50 -0700

    indexes implemented

--- ui/src/app/components/panels/indexes-cards/indexes-cards.ts
+++ ui/src/app/components/panels/indexes-cards/indexes-cards.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { GeneralIndexCard } from "./general-index-card/general-index-card";
 import { MATERIAL_IMPORTS } from '../../../material-imports';
+import { LiveDataService } from '../../../services/live-data/live-data-service';
+import { catchError, of, retry } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TmytsSnackbar } from '../../sub-components/tmyts-snackbar/tmyts-snackbar';
 
-  export type IndexCardInterface= {
-    id: string;
-    name: string;
-    points: number;
-    variation: number;
-    percent: number
-  }
+export type IndexCardInterface = {
+  id: string;
+  name: string;
+  points: number;
+  variation: number;
+  percent: number
+}
+
+export type IndexNameType = {
+  id: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-indexes-cards',
@@ -19,13 +28,67 @@ import { MATERIAL_IMPORTS } from '../../../material-imports';
   templateUrl: './indexes-cards.html',
   styleUrl: './indexes-cards.scss'
 })
-export class IndexesCards {
+export class IndexesCards implements OnInit {
 
-  INDEXES: IndexCardInterface[] = [
-    {id: '^DJI', name: 'Dow Jones', points: 46247.29, variation: 299.97, percent: 0.0065},
-    {id: '^GSPC', name: 'S&P 500', points: 6643.70, variation: 38.98, percent: 0.0065},
-    {id: '^IXIC', name: 'NASDAQ', points: 22484.07, variation: -99.37, percent: -0.0044},
-    {id: '^GSPTSE', name: 'TSX', points: 29761.28, variation: 29.30, percent: 0.0099},
-    {id: '^BVSP', name: 'IBOVESPA', points: 145446.66, variation: -140.42, percent: -0.0010},
+  liveDataService = inject(LiveDataService)
+
+  INDEXES: IndexCardInterface[] = []
+
+  INDEXES_NAME: IndexNameType[] = [
+    { id: '^DJI', name: 'Dow Jones' },
+    { id: '^GSPC', name: 'S&P 500' },
+    { id: '^IXIC', name: 'NASDAQ' },
+    { id: '^GSPTSE', name: 'TSX' },
+    { id: '^BVSP', name: 'IBOVESPA' },
   ]
+
+  constructor(private _snackBar: MatSnackBar) {
+  }
+
+  ngOnInit(): void {
+    const indexesList = this.INDEXES_NAME.map(
+      row => row.id
+    );
+    this.liveDataService.getIndexesData(indexesList)
+      .pipe(
+        // retry(3),
+        catchError(
+          (error) => {
+            // Handle error response
+            const message: string = `Error: ${JSON.stringify(error.error.detail)}`;
+
+            // Renders error snack-bar
+            this._snackBar.openFromComponent(
+              TmytsSnackbar, {
+              data: { 'message': message, 'action': 'Close' },
+              panelClass: ['error-snackbar-theme']
+            }
+            );
+            return of([]);
+          }
+        )
+      )
+      .subscribe(
+        {
+          next: (response: IndexCardInterface[]) => {
+
+            response.map(item1 => {
+              const match = this.INDEXES_NAME.find(item2 => item2.id === item1.id);
+
+              if (match) {
+                // If a match is found, replace the `value`
+                item1.name = match.name
+                return { ...item1 };
+              }
+
+              // Otherwise, return the original item
+              return item1;
+            });
+
+            // console.log(updatedArray)
+            response.forEach(row => this.INDEXES.push(row))
+          }
+        }
+      );
+  }
 }
