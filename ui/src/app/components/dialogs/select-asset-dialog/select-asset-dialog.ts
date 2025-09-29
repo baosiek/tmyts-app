a1ea24cc651e314d8553eaed93f8d1426c3a9f9c
baosiek <baosiek@gmail.com>
2025-09-29 10:59:43 -0700

    file created

---
+++ ui/src/app/components/dialogs/select-asset-dialog/select-asset-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-asset-dialog',
+  imports: [],
+  templateUrl: './select-asset-dialog.html',
+  styleUrl: './select-asset-dialog.scss'
+})
+export class SelectAssetDialog {
+
+}
