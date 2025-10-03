be7831393d7fa9cf4ed45208d646897c1c8178a0
baosiek <baosiek@gmail.com>
2025-10-03 15:18:20 -0700

    created

---
+++ ui/src/app/components/panels/dashboards/assets-analysis/asset-analysis-widgets/aroon-widget/aroon-widget.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aroon-widget',
+  imports: [],
+  templateUrl: './aroon-widget.html',
+  styleUrl: './aroon-widget.scss'
+})
+export class AroonWidget {
+
+}
