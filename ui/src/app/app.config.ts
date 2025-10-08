97be2de8af082c325d730342831ca9034be23de7
baosiek <baosiek@gmail.com>
2025-10-07 19:29:44 -0700

    highcharts installed

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,12 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-
+import { provideHighcharts } from 'highcharts-angular';
 import { routes } from './app.routes';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideHighcharts(),
     provideNativeDateAdapter(),
      {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false, maxWidth: '90vw', maxHeight: '90vh'}},
      {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 5000}}
