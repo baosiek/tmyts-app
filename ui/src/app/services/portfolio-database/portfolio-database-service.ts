e46c92e6fc332d3217a408e81a4dcb17f714c827
baosiek <baosiek@gmail.com>
2026-04-28 10:50:34 -0700

    get portfolioHoldings inserted

--- ui/src/app/services/portfolio-database/portfolio-database-service.ts
+++ ui/src/app/services/portfolio-database/portfolio-database-service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PortfolioModel } from '../../models/portfolio-model';
+import { PortfolioHoldingsModel } from '../../models/portfolio_holdings_model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,25 @@ import { PortfolioModel } from '../../models/portfolio-model';
 export class PortfolioDatabaseService {
 
   http = inject(HttpClient)
-  apiUrl = 'http://127.0.0.1:8000/portfolios';
+  apiUrl = 'http://127.0.0.1:8000';
 
   constructor() { }
 
   createPortfolio(portfolio_data: Partial<PortfolioModel>): Observable<PortfolioModel> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    const apiMethod = 'create';
+    const apiMethod = 'portfolios/create';
     const body = portfolio_data;
 
     return this.http.post<PortfolioModel>(`${this.apiUrl}/${apiMethod}/`, body, { headers })
   }
 
   readAllPortfolios(user_id: number): Observable<PortfolioModel[]> {
-    const apiMethod = 'get_all';
+    const apiMethod = 'portfolios/get_all';
     return this.http.get<PortfolioModel[]>(`${this.apiUrl}/${apiMethod}/${user_id}/`)
+  }
+
+  getPortfolioHoldings(user_id: number, portfolio_name: string): Observable<PortfolioHoldingsModel[]> {
+    const apiMethod = 'portfolio_holdings/holdings';
+    return this.http.get<PortfolioHoldingsModel[]>(`${this.apiUrl}/${apiMethod}/${user_id}/${portfolio_name}`)
   }
 }
