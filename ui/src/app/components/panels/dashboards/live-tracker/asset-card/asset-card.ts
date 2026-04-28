732911253144c6afc55a271cc9be78f97f54e252
baosiek <baosiek@gmail.com>
2026-04-28 16:18:56 -0700

    highcharts being rendered adequatly

--- ui/src/app/components/panels/dashboards/live-tracker/asset-card/asset-card.ts
+++ ui/src/app/components/panels/dashboards/live-tracker/asset-card/asset-card.ts
@@ -1,12 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import * as Highcharts from 'highcharts';
+import { HighchartsChartDirective } from "highcharts-angular";
 import { PortfolioHoldingsModel } from '../../../../../models/portfolio_holdings_model';
-import { HighchartsChartComponent } from "highcharts-angular";
 
 @Component({
   selector: 'app-asset-card',
-  imports: [MatCardModule, HighchartsChartComponent],
+  imports: [MatCardModule, HighchartsChartDirective],
   templateUrl: './asset-card.html',
   styleUrl: './asset-card.scss'
 })
@@ -16,15 +16,35 @@ export class AssetCard {
    * Class variables
    */
   asset = input.required<PortfolioHoldingsModel>();
+  updateFlag: boolean = false
 
   Highcharts: typeof Highcharts = Highcharts;
 
-  chartOptions: Highcharts.Options = {
-    title: { text: 'Angular 21 Line Chart' },
+  componentColor = getComputedStyle(document.documentElement).getPropertyValue('--mat-sys-surface').trim()
+
+  // Define chart options as a signal
+  chartOptions = signal<Highcharts.Options>({
+    title: { text: undefined },
+    chart: {
+      backgroundColor: this.componentColor,
+      styledMode: false,
+      height: null
+    },
+    legend: { enabled: true },
+    credits: { enabled: false },
+    tooltip: { enabled: false },
+    plotOptions: {
+      series: {
+        marker: { enabled: true },
+        lineWidth: 4,
+        states: { hover: { lineWidth: 4 } },
+        color: '#1976d2'
+      }
+    },
     series: [{
-      data: [1, 2, 3, 4, 5],
-      type: 'line'
+      type: 'line',
+      data: [1, 3, 2, 4]
     }]
-  };
+  });
 
 }
