f026a4dd8a9edd1b50663bbdfb0dcb0ce72ae1e1
baosiek <baosiek@gmail.com>
2026-04-28 12:39:41 -0700

    started developing card

--- ui/src/app/components/panels/dashboards/live-tracker/asset-card/asset-card.ts
+++ ui/src/app/components/panels/dashboards/live-tracker/asset-card/asset-card.ts
@@ -1,9 +1,12 @@
 import { Component, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import * as Highcharts from 'highcharts';
 import { PortfolioHoldingsModel } from '../../../../../models/portfolio_holdings_model';
+import { HighchartsChartComponent } from "highcharts-angular";
 
 @Component({
   selector: 'app-asset-card',
-  imports: [],
+  imports: [MatCardModule, HighchartsChartComponent],
   templateUrl: './asset-card.html',
   styleUrl: './asset-card.scss'
 })
@@ -13,5 +16,15 @@ export class AssetCard {
    * Class variables
    */
   asset = input.required<PortfolioHoldingsModel>();
+
+  Highcharts: typeof Highcharts = Highcharts;
+
+  chartOptions: Highcharts.Options = {
+    title: { text: 'Angular 21 Line Chart' },
+    series: [{
+      data: [1, 2, 3, 4, 5],
+      type: 'line'
+    }]
+  };
 
 }
